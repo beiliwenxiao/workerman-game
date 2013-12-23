cf590280acbd29dda271748cf83af62290f78504
walkor <worker-man@qq.com>
2013-12-23 13:59:13 +0800

    加一些注释

--- applications/Game/Event.php
+++ applications/Game/Event.php
@@ -10,6 +10,12 @@ require_once WORKERMAN_ROOT_DIR . 'applications/Game/Store.php';
 
 class Event
 {
+    /**
+     * 当有用户连接时，会触发该方法
+     * @param string $address 和该用户gateway通信的地址
+     * @param integer $socket_id 该用户链接的socketid
+     * @param string $sid sessionid或者说是客户端id
+     */
    public static function onConnect($address, $socket_id, $sid)
    {
        // 检查sid是否合法
@@ -21,13 +27,23 @@ class Event
            return;
        }
        
-       // 合法记录uid到address的映射
+       // 合法，记录uid到gateway通信地址的映射
        self::storeUidAddress($uid, $address);
        
-       // 发送数据包到address，确认connection成功
+       // 发送数据包到address对应的gateway，确认connection成功
        self::notifyConnectionSuccess($address, $socket_id, $uid);
+       
+       /**
+        * 业务的其它逻辑
+        * 。。。。。。。。
+        */
    }
    
+   /**
+    * 当用户断开连接时触发的方法
+    * @param 和该用户gateway通信的地址 $address
+    * @param integer $uid 用户id 
+    */
    public static function onClose($address, $uid)
    {
        $buf = new Gamebuffer();
@@ -35,7 +51,9 @@ class Event
        $buf->header['sub_cmd'] = GameBuffer::SCMD_BROADCAST;
        $buf->header['from_uid'] = $uid;
        $buf->body = "logout bye!!!";
+       // 广播所有人，这个用户退出了
        GameBuffer::sendToAll($buf->getBuffer());
+       // 删除这个用户的gateway通信地址
        self::deleteUidAddress($uid);
    }
    
